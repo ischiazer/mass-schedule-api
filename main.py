f8a5b96839f685917c771ed0ab3cf06e14ca9b14
ischiazer <etienne.comon@googlemail.com>
2025-06-02 16:28:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, send_file, Response, send_file
 from bs4 import BeautifulSoup
 import io
 import nest_asyncio
+import pandas as pd, numpy as np
 import asyncio
 import zipfile
 from playwright.async_api import async_playwright
@@ -885,6 +886,100 @@ def get_news():
 
 
 ##################################################################
+# SUB-FUNCTION - RETURN LIST OF CITIES FOR WHICH TEMPERATURE IS NEEDED
+
+def get_city_mapping():
+    mapping_city_url = {'Den Haag': "https://www.seatemperature.org/europe/netherlands/scheveningen.htm",
+                        "Egmond":"https://www.seatemperature.org/europe/netherlands/egmond-aan-zee.htm",
+                        "Vlieland":"https://www.seatemperature.org/europe/netherlands/oost-vlieland.htm",
+                        "Knokke":"https://www.seatemperature.org/europe/belgium/knokke-heist.htm",
+                        "Penzance":"https://www.seatemperature.org/europe/united-kingdom/penzance.htm",
+                        "Falmouth":"https://www.seatemperature.org/europe/united-kingdom/falmouth.htm",
+                        "Brighton": "https://www.seatemperature.org/europe/united-kingdom/brighton.htm",
+                        "Helgoland":"https://www.seatemperature.org/europe/germany/helgoland.htm",
+                        "Saint Malo": "https://www.seatemperature.org/europe/france/saint-malo.htm",
+                        "Cancale":"https://www.seatemperature.org/europe/france/cancale.htm",
+                        "Saint Lunaire":"https://www.seatemperature.org/europe/france/saint-lunaire.htm",
+                        "Cabourg":"https://www.seatemperature.org/europe/france/cabourg.htm",
+                        "Honfleur":"https://www.seatemperature.org/europe/france/honfleur.htm",
+                        "Benodet":"https://www.seatemperature.org/europe/france/benodet.htm",
+                        "Quiberon": "https://www.seatemperature.org/europe/france/quiberon.htm",
+                        "Saint Jean de Luz":"https://www.seatemperature.org/europe/france/saint-jean-de-luz.htm",
+                        "Lavandou":"https://www.seatemperature.org/europe/france/le-lavandou.htm",
+                        "Antibes":"https://www.seatemperature.org/europe/france/antibes.htm",
+                        "Carqueiranne": "https://www.seatemperature.org/europe/france/carqueiranne.htm",
+                        "Biarritz":"https://www.seatemperature.org/europe/france/biarritz.htm",
+                        "Ile Rousse":"https://www.seatemperature.org/europe/france/lile-rousse.htm",
+                        "Toulon": "https://www.seatemperature.org/europe/france/toulon.htm",
+                        "Sorrento":"https://www.seatemperature.org/europe/italy/sorrento.htm",
+                        "Rapallo":"https://www.seatemperature.org/europe/italy/rapallo.htm",
+                        "Ischia":"https://www.seatemperature.org/europe/italy/ischia.htm",
+                        "Catania":"https://www.seatemperature.org/europe/italy/catania.htm",
+                        "Heraklion":"https://www.seatemperature.org/europe/greece/irkleion.htm",
+                        "Rhodes":"https://www.seatemperature.org/europe/greece/rodos.htm",
+                        "Corfu":"https://www.seatemperature.org/europe/greece/corfu.htm",
+                        "Cascais":"https://www.seatemperature.org/europe/portugal/cascais.htm",
+                        "Albufeira":"https://www.seatemperature.org/europe/portugal/albufeira.htm",
+                        }
+    return mapping_city_url
+
+##################################################################
+# SUB-FUNCTION - GET FROM WEB HTML FILE WITH TEMPERATURE FOR ONE CITY
+
+async def temperature_fetch_full_text(city):
+    url = get_city_mapping()[city]
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)
+        page = await browser.new_page()
+        await page.goto(url)
+        await page.wait_for_selector("body")  # Wait for the main body to load
+        return await page.inner_text("body")
+
+##################################################################
+# SUB-FUNCTION - GET FROM WEB CURRENT TEMPERATURE
+def temperature_current():
+    dt_str = datetime.today().strftime('%Y-%m-%d')
+    temps = pd.DataFrame(index=[k for k in get_city_mapping()], columns=[dt_str])
+    for city in temps.index:
+        print(city)
+        html = run_async(temperature_fetch_full_text(city))
+        html = html[html.index('(Today)'):]
+        html = html[:html.index('°C')]
+        html = html[html.index('\n')+1:]
+        try:
+            t = float(html)
+        except:
+            t = np.nan
+        temps.loc[city, dt_str] = t
+    return temps
+
+##################################################################
+# FUNCTION - UPDATE SEA TEMPERATURE FILE
+def update_temperatures():
+    # Load existing history of temperatures
+    download_file_from_b2(TEMPERATURE_CSV, TEMPERATURE_CSV)
+    temps_existing = pd.read_csv(TEMPERATURE_CSV,index_col=0)
+
+    # Get current temperatures
+    temps_new = temperature_current()
+
+    # Add current temperatures to existing
+    if temps_new.columns[0] in temps_existing.columns:
+        print('Date %s already present' % temps_new.columns[0])
+        temps_updated = temps_existing.copy()
+    else:
+        temps_updated = temps_new.join(temps_existing, how='outer')
+
+    # Save and upload
+    temps_updated.to_csv(TEMPERATURE_CSV)
+    push_b2_file(TEMPERATURE_CSV,TEMPERATURE_CSV)
+
+##################################################################
+# FUNCTION: CALL THE SEA TEMPERATURE UPDATE
+def force_fetch_temperature():
+    update_temperature()
+
+##################################################################
 # QUERY - FETCH VATICAN NEWS
 @app.route('/fetch_vatican_news')
 def force_fetch_vatican_news():
@@ -907,6 +1002,14 @@ def periodic_query_perplexity():
     while True:
         force_fetch_perplexity()
         time.sleep(12 * 60 * 60)
+
+##################################################################
+# REGULAR CALL TO THE SEA TEMPERATURE
+def periodic_query_temperature():
+    while True:
+        force_fetch_temperature()
+        time.sleep(60 * 60 * 12)
+
 
 ##################################################################
 # QUERY - STATIC PERPLEXITY NEWS
