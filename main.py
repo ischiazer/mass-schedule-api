479f7ec4c9a695aea5c56c73b1decbe2d5106f11
ischiazer <etienne.comon@googlemail.com>
2025-06-22 22:56:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,17 +23,15 @@ print('*app created*')
 ##################################################################
 # SCHEDULE TASKS
 def start_background_threads():
-    if False:
-        # Define functions to be called at regular intervals
-        for func in []: #[background_loop_temperature, periodic_query_readings, periodic_query_vatican_news, periodic_query_perplexity]:
-            try:
-                logging.info(f"Starting background thread: {func.__name__}")
-                thread = threading.Thread(target=func, name=func.__name__, daemon=True)
-                thread.start()
-            except Exception as e:
-                logging.error(f"Failed to start thread {func.__name__}: {e}")
-    else:
-        print('No background\n\n\n')
+    # Define functions to be called at regular intervals
+    for func in [background_loop_temperature, periodic_query_readings, periodic_query_vatican_news, periodic_query_perplexity]:
+        try:
+            logging.info(f"Starting background thread: {func.__name__}")
+            print(f"--Starting background thread: {func.__name__}")
+            thread = threading.Thread(target=func, name=func.__name__, daemon=True)
+            thread.start()
+        except Exception as e:
+            logging.error(f"Failed to start thread {func.__name__}: {e}")
 
 ##################################################################
 # MAIN
@@ -44,13 +42,15 @@ def start_background_threads():
     start_background_threads()
     logging.info('...done')
     is_local = False
-    if not (os.getenv("LOCAL_LAPTOP") is None):
-        if os.getenv("LOCAL_LAPTOP") != '':
+    env_var = os.getenv("LOCAL_LAPTOP")
+    if not (env_var is None):
+        if env_var != '':
             is_local = True
+            print('//-- local details: --->.  <' + str(env_var) + '>')
     if is_local:
         print('* is local *')
-        print('type of environment variable: '+str(type(os.getenv("LOCAL_LAPTOP"))))
-        print('value of environment variable: <'+str(os.getenv("LOCAL_LAPTOP")) + '>')
+        print('type of environment variable: '+str(type(env_var)))
+        print('value of environment variable: <'+str(env_var) + '>')
         logging.info("Starting Flask server locally on port 5050")
         app.run(debug=True, port=5050)
         logging.info("Flask server started")
