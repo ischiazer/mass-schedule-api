d0a40eff9b348f1bd6033ae74c4dd57de20f0f77
ischiazer <etienne.comon@googlemail.com>
2025-07-06 00:07:55 +0200

    a

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-from app.utilities import log_msg
+from utilities import log_msg
 
 def post_fork(server, worker):
     from background_threads import start_background_threads
